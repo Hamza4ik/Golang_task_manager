7d6985e048f4455f4456c8ac2885189da9cc0317
unknown <hamzamailcom573@gmail.com>
2026-07-26 12:43:39 +0700

    feat: add mark and delete commands

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/Hamza4ik/Golang_task_manager/internal/storage"
@@ -13,7 +14,7 @@ import (
 var tasks []task.Task
 
 func ReadInp() {
-	fmt.Println("Write command: add, list, exit")
+	fmt.Println("Write command: add,  list, mark, delete, exit")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -30,6 +31,34 @@ func ReadInp() {
 			task.AddTask(&tasks, task.Task{ID: len(tasks) + 1, Text: text, Done: false})
 			if err := storage.SaveTasks(tasks); err != nil {
 				fmt.Println("Не удалось сохранить")
+			}
+		case "mark":
+			if len(parts) < 2 {
+				fmt.Println("Len less then 2")
+				continue
+			}
+			id, err := strconv.Atoi(parts[1])
+			if err != nil {
+				fmt.Println("Id must be int")
+				continue
+			}
+			task.MarkDone(&tasks, id)
+			if err := storage.SaveTasks(tasks); err != nil {
+				fmt.Println("Не удалось сохранить")
+			}
+		case "delete":
+			if len(parts) < 2 {
+				fmt.Println("Len less then 2")
+				continue
+			}
+			id, err := strconv.Atoi(parts[1])
+			if err != nil {
+				fmt.Println("Id must be int")
+				continue
+			}
+			task.DeleteTask(&tasks, id)
+			if err := storage.SaveTasks(tasks); err != nil {
+				fmt.Println("Failed to save")
 			}
 		case "list":
 			task.ListTasks(tasks)
